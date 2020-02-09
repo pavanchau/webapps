91977d111a260b6705284e9c3eb5285f045d2185
khema <khema@DESKTOP-6AC4PJ8>
2020-02-09 12:15:56 +0530

    1st file change

---
+++ src/com/nt/sample/Product.java
@@ -0,0 +1,22 @@
+package com.nt.sample;
+
+public class Product {
+	
+	private int pid;
+	private String pcode;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPcode() {
+		return pcode;
+	}
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+	
+	
+
+}
